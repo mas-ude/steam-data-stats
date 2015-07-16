cb6479a433360ef5a0a09783168b233c6d3c7798
Florian Metzger <fm@tru42.org>
2015-07-16 16:27:26 +0200

    categorization and nice violin and box plots

--- steamAPIdata-plotting.R
+++ steamAPIdata-plotting.R
@@ -40,30 +40,38 @@ ggplot(df.priced, aes(x=price, y=players_forever)) + geom_bin2d() + scale_x_log1
 
 
 ### custom binning and setting levels
-for (i in 1:nrow(d)) {
-  Sys.sleep(2) # steam storefront API is throttled to ~200requests/5mins
-  row <- d[i,]
-  print(paste("http://store.steampowered.com/api/appdetails/?appids=",row["appid"], sep=""))
-  tmp <- fromJSON(paste("http://store.steampowered.com/api/appdetails/?appids=",row["appid"], sep=""))
+df.priced2 <- data.frame()
+for (i in 1:nrow(df.priced)) {
+  row <- df.priced[i,]
   
-  price <- tmp[[paste(row[["appid"]])]]$data$price_overview$final
-  print(price)
-  
-  if(!is.null(price)) {
-    row$price <- price
-  } else { # TODO: additionally check for is.free here!
-    row$price <- 0
-  }
-  
+  if(row$price == 0) {
+    row$price_category <- "free"
+  } else if (row$price > 0 & row$price <= 500){
+    row$price_category <- "<5"
+  } else if (row$price > 500 & row$price <= 1000) {
+    row$price_category <- "5 to 10"
+  } else if (row$price > 1000 & row$price <= 2000) {
+    row$price_category <- "10 to 20"
+  } else if (row$price > 2000 & row$price <= 4000) {
+    row$price_category <- "20 to 40"
+  } else if (row$price > 4000) {
+    row$price_category <- "above 40"
+  }  
   tmp <- data.frame(row)
-  df.priced <- rbind(df.priced, tmp)
+  df.priced2 <- rbind(df.priced2, tmp)
 }
+df.priced2$price_category <- as.factor(df.priced2$price_category)
+df.priced2$price_category <- ordered(df.priced2$price_category, levels=c("free", "<5", "5 to 10", "10 to 20", "20 to 40", "above 40"))
+ggplot(df.priced2, aes(x=price_category, y=players_forever)) + geom_violin() + scale_y_log10()
+ggsave("dampfviolinen-players.pdf")
+ggplot(df.priced2, aes(x=price_category, y=average_forever)) + geom_violin() + scale_y_log10()
+ggsave("dampfviolinen-playtime.pdf")
 
+ggplot(df.priced2, aes(x=price_category, y=average_forever)) + geom_boxplot() + scale_y_log10()
 
 
 
 d1 <- subset(df.priced, price==0  | (price>1000))
-
 graph <- ggplot(d1, aes(x=average_forever, color=as.factor(sign(price)))) 
 graph + stat_ecdf() + scale_x_log10()
 
