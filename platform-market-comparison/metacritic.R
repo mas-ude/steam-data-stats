26fac205825d3a5592e007b3bcda1f8496b33e6b
Florian Metzger <fm@tru42.org>
2015-12-27 12:46:52 +0100

    merge metacritic and psnow datasets - fixed up title names in psnow set - calculate score and age of psnow titles - import new metacritic scrape

--- platform-market-comparison/metacritic.R
+++ platform-market-comparison/metacritic.R
@@ -3,9 +3,10 @@ library(plyr)
 
 ### load and analyse metacritic data
 # Note: file not provided here
-# scrape the date with https://github.com/mas-ude/metacritic_scraper
+# scrape the data with https://github.com/mas-ude/metacritic_scraper
 # and place it in the data folder
 
+# setwd("git/steam-data-stats/platform-market-comparison/")
 
 df.metacritic <- read.csv("data/metacritic.csv", header=TRUE, sep=";", colClasses=c("numeric", "character", "character", "character", "numeric", "character", "character"))
 df.metacritic$platform <- as.factor(df.metacritic$platform)
@@ -17,6 +18,23 @@ ggplot(df.metacritic, aes(x=score, color=platform)) + stat_density(position = "d
 ## quick plot for releases per year
 #df.metacritic$month = strftime(df.metacritic$release, "%b")
 df.metacritic$year = strftime(df.metacritic$release, "%Y")
-releases.per-year = count(df.metacritic, vars = c("year", "platform"))
-ggplot(releases.per-year, aes(x=year, y=freq, fill=platform)) + geom_bar(stat="identity", position = "stack")
+releases.peryear = count(df.metacritic, vars = c("year", "platform"))
+ggplot(releases.peryear, aes(x=year, y=freq, fill=platform)) + geom_bar(stat="identity", position = "stack")
 ggsave("releases-per-year.pdf")
+
+
+##############
+df.psnow <- read.csv("data/psnow-games.csv", sep = ";", colClasses = c("character", "numeric", "numeric", "numeric", "numeric", "logical"))
+df.metacritic.ps3 <- subset(df.metacritic, platform == "ps3")
+df.merged <- merge(df.psnow, df.metacritic.ps3, by.x = "Title", by.y = "title", all.x = TRUE)
+
+## scores
+mean(df.merged$score, na.rm = TRUE)
+
+## age
+df.merged$year <- as.numeric(strftime(df.merged$release, "%Y"))
+df.merged$age <- 2015 - df.merged$year
+
+hist(df.merged$age)
+mean(df.merged$age, na.rm = TRUE)
+median(df.merged$age, na.rm = TRUE)
